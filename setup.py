0ca86a44326bf2d057f64326c073dbc84431ead2
Santiago Lezica <slezica89@gmail.com>
2014-03-07 17:39:35 -0300

    Added info to setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,13 @@ from distutils.core import setup
 
 setup(
     name     = 'sel',
-    version  = '0.2',
+    url      = 'http://github.com/slezica/sel',
+    version  = '0.2.2',
     packages = ['sel'],
-    scripts  = ['bin/sel']
+    scripts  = ['bin/sel'],
+
+    author       = 'Santiago Lezica',
+    author_email = 'slezica89@gmail.com',
+
+    description = 'A field selection command-line tool',
 )
